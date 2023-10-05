4fb66cbcf0c1ce3322cd60dfd85b98bb58de9ca5
Shashank <shashank.shri10@gmail.com>
2023-10-05 11:10:05 +0530

    .

--- sim.py
+++ sim.py
@@ -9,3 +9,7 @@ def genrn ():
     return r1
 r1 = genrn()
 # now slice the array from els range(0,4) and range(4,9)
+iat = [stats.expon.ppf(r1[i],scale = 0.2) for i in range(0,4)]
+w = [24,76,0]
+bin_edges = [0,5,10,15]
+st = [stats.rv_histogram((w,bin_edges)).ppf(r1[i]) for i in range(4,9)]
