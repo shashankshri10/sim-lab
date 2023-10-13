1e5d5ee9be8104d6e83785b49ab2ff9b9305e232
Shashank <shashank.shri10@gmail.com>
2023-10-13 12:41:03 +0530

    Stats generated wubba lubba dub dub

--- sim2.py
+++ sim2.py
@@ -1,24 +1,46 @@
 import simpy
 import scipy.stats as stats
 from lcg import lcg
+import numpy as np
 
-lines = []
+lines = ["Event List Part 2\n"]
+wtm=[0 for i in range(0,100)]
+absarr=[0 for i in range(0,100)]
+ttm=[0 for i in range(0,100)]
+pcus=0
+ncus=0
+priority_cus_ind=[]
+simstats=["---Simulation Report Part 2---\n"]
 
 def checkIn(env,cind,pty,counters,st,iat):
     # cind is cutomer index, ctype is priority or normal customer
     ctype="high" if (pty==0) else "normal"
+    global ncus
+    global pcus
+    global wtm
+    global absarr
+    global ttm
+    global priority_cus_ind
     tm=0
+    if (pty==1):
+        ncus+=1
+    else:
+        pcus+=1
+        priority_cus_ind.append(cind)
     for i in range(0,cind):
         tm=tm+iat[i]
     if (cind != 0):
         yield env.timeout(tm)
-    lines.append('Customer %d with %s prority arrived at %.2f'%(cind+1,ctype,env.now))
-    print('Customer %d with %s prority arrived at %.2f'%(cind+1,ctype,env.now))
+    absarr[cind]=env.now
+    lines.append('Customer %d with %s priority arrived at %.2f'%(cind+1,ctype,env.now))
+    print('Customer %d with %s priority arrived at %.2f'%(cind+1,ctype,env.now))
     with counters.request(priority=pty) as req:
         yield req
+        wtm[cind]=env.now-absarr[cind] # wait time in queue
         lines.append('Customer %d with %s priority started check in at %.2f'%(cind+1,ctype,env.now))
         print('Customer %d with %s priority started check in at %.2f'%(cind+1,ctype,env.now))
         yield env.timeout(st[cind])
+        ttm[cind]=env.now-absarr[cind] # total time = wait tm + service tm
         lines.append('Customer %d with %s priority finished check in and leaving at %.2f'%(cind+1,ctype,env.now))
         print('Customer %d with %s priority finished check in and leaving at %.2f'%(cind+1,ctype,env.now))
 
@@ -52,4 +74,33 @@ env.run()
 print('\n---Simulation finished---\n')
 with open('sim2res.txt','w') as f:
     f.write('\n'.join(lines))
+    f.close
+
+def sim_stats(simstats):
+    simstats.append('Average wait time = %.2f'% (np.mean(wtm)))
+    simstats.append('Maximum wait time = %.2f'% (np.max(wtm)))
+    simstats.append('Average total time = %.2f'% (np.mean(ttm)))
+    simstats.append('Maximum total time = %.2f'% (np.max(ttm)))
+    simstats.append('Total run time of the simulation = %.2f' %(ttm[4]+absarr[4]))
+    simstats.append('Number of normal customers = %d'% (ncus))
+    simstats.append('Number of Priority customers = %d'% (pcus))
+    if (len(priority_cus_ind)!=0):
+        val=0
+        mval=-1
+        for i in range(0,len(priority_cus_ind)):
+            val+=wtm[priority_cus_ind[i]]
+            mval=max(mval,wtm[priority_cus_ind[i]])
+        val/=len(priority_cus_ind)
+        simstats.append('Average wait time for priority customers = %.2f'% (val))
+        simstats.append('Maximum wait time for priority customers = %.2f'% (mval))
+    else:
+        simstats.append('Average wait time for priority customers = N/A')
+        simstats.append('Average wait time for priority customers = N/A')
+        # print(absarr)
+    print('\n---Statistics generated---\n')
+    return simstats
+
+simstats = sim_stats(simstats)
+with open('sim2stats.txt','w') as f:
+    f.write('\n'.join(simstats))
     f.close
